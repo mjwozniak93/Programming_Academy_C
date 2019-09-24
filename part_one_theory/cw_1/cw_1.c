55d061bf675619c6f60daac6efd79a650558a85c
Maciek <mjwozniak93@gmail.com>
2019-09-24 20:41:30 +0200

    Added new files: cw_1, cw_2, cw_3 according to Kurs_C_cz1.pdf

---
+++ part_one_theory/cw_1/cw_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // First
+    printf("Moj pierwszy program\n\n");
+
+    // Second
+    printf("0 1 2 3 4 5 6 7 8 9\n\n");
+
+    // Third
+    printf("0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n");
+    
+    system("PAUSE");
+    return 0;
+}
---
+++ part_one_theory/cw_2/cw_2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    // First
+    printf("int: %d \r\n", sizeof(int));
+    printf("float: %d \r\n", sizeof(float));
+    printf("double: %d \r\n", sizeof(double));
+    printf("char: %d \r\n", sizeof(char));
+    printf("bool: %d \r\n\n", sizeof(bool));
+
+    // Second
+    printf("Wyswietlam liczbe: %d\n\n",4);
+
+    // Third
+    printf("%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n",0,1,2,3,4,5,6,7,8,9);
+
+    system("PAUSE");
+    return 0;
+}
